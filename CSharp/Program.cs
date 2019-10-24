c0f648806a9d9cdb56ef5f6368846b13daed8245
Benji Altman <benji@altmansoftwaredesign.com>
2019-10-24 10:56:31 -0500

    Add project files.

---
+++ CSharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net.Http;
+
+namespace CSharp
+{
+	class Program
+	{
+		public static readonly HttpClient http = new HttpClient();
+
+		static void Main(string[] args)
+		{
+			var formatStr = "blahblah{0}";
+			Console.WriteLine(formatStr, 1);
+			Console.WriteLine(formatStr, 2);
+		}
+	}
+}
